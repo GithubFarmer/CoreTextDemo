552f9ef593417084f62364fa69f48dd8b3f33344
rainy <rainy.yu@wwwarehouse.com>
2018-12-17 16:56:13 +0800

    Initial Commit

---
+++ CoreTextDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreTextDemo
+//
+//  Created by 喻永权 on 2018/12/17.
+//  Copyright © 2018年 喻永权. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
